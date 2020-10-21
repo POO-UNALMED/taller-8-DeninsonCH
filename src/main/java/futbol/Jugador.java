14069e6b16ba2de042775d181adea536a9fc01cf
DeninsonCH <69688198+DeninsonCH@users.noreply.github.com>
2020-10-20 22:44:23 -0500

    punto b

--- src/main/java/futbol/Jugador.java
+++ src/main/java/futbol/Jugador.java
@@ -21,5 +21,34 @@ public class Jugador extends Futbolista{
 		golesMarcados=27;
 		dorsal=7;
 	}
+	public int compareTo(Object j) {
+		Jugador temp  = (Jugador) j;
+		int difEdad = this.getEdad() - temp.getEdad();
+		return difEdad;
+	}
+	
+	public String toString() {
+		String salida= String.format("El futbolista %s tiene %d, y juega de %s con el dorsal %d. Ha marcado %s",this.getNombre(),this.getEdad(),this.getPosicion(),this.dorsal,this.golesMarcados);
+		return salida;
+	}
+	public short getGolesMarcados() {
+		return golesMarcados;
+	}
+	public void setGolesMarcados(short golesMarcados) {
+		this.golesMarcados = golesMarcados;
+	}
+	public byte getDorsal() {
+		return dorsal;
+	}
+	public void setDorsal(byte dorsal) {
+		this.dorsal = dorsal;
+	}
+	@Override
+	public boolean jugarConLasManos() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
 
 }
